922ba8765f16d997b58226ab6750b8fb90799f6a
Barankov Anton <barankov-av@yandex.ru>
2023-11-28 20:02:12 +0300

    welcomer

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,4 +13,19 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
+	public String sayHunter() {
+    String[] hunterPhrases = {
+        "May the good blood guide your way, hunter.",
+        "You're a hunter, aren't you? I knew it. That's precisely how I started out!",
+        "Ah, sweet child of blood, hunter of beasts, what's left of you now?",
+        "A hoonter must hoont.",
+        "You've come to the right place. A hoonter must hoont.",
+        "You're a hunter with your sanity, aren't you?",
+        "A hoonter must hoont. But first, you'll need some blood.",
+        "You're a hunter, aren't you? You must not fear the beasts. They are of no harm to those who hunt them."
+    };
+    Random random = new Random();
+    int index = random.nextInt(hunterPhrases.length);
+    return hunterPhrases[index];
+    }
 }
